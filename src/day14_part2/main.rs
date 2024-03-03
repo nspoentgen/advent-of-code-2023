d39efd4bad3ffef8ca3633b2c197d55093e71a70
Nick Spoentgen <157921556+nspoentgen@users.noreply.github.com>
2024-03-03 13:44:09 -0800

    Cleaned up day 14 part 2 implementation

--- src/day14_part2/main.rs
+++ src/day14_part2/main.rs
@@ -1,54 +1,51 @@
-use std::collections::hash_map::Entry;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::path::Path;
 use std::collections::HashMap;
+use core::ops::Range;
 use itertools::Itertools;
 use num_format::{Locale, ToFormattedString};
-
-#[macro_use]
-extern crate timeit;
 
 const FIXED: char = '#';
 const MOVABLE: char = 'O';
 const EMPTY: char = '.';
 
+enum ShiftDirection {
+    NORTH,
+    WEST,
+    SOUTH,
+    EAST
+}
+
 fn main() {
+    //Constants
+    const NUM_CYCLES: usize = 1000000000;
+    const LIMIT_CYCLE_OFFSET: usize = 92;
+    const LIMIT_CYCLE_LENGTH: usize = 72;
+
     //Parse data
     let path = Path::new("src/day14_part1/input.txt");
     let mut data = parse_data(&path);
-    /*
-        let mut cache = HashMap::<Vec<Vec<char>>, usize>::new();
 
-   match cache.get(&data) {
-       Some(value) => {
-           println!("Cycle {} repeat of cycle {}", cycle, value);
-       },
-       None => {
-           cache.insert(data.clone(), cycle);
-       }
-   };
-     */
-    //0-indexing
-    //92-163 inclusive cycle
-
+    //We can't shift for 1 billion cycles, so we need to find a shortcut. Looking at the data,
+    //we see there is a limit cycle in the rock data. The limit cycle has an offset of 92 and
+    //a length of 72 (using 0-based indexing). Cache all the possiblilties using the
+    //module limit cycle number for the cycle
     let mut cache = HashMap::<usize, Vec<Vec<char>>>::new();
-    for cycle in 0usize..=163 {
-        shift_north(&mut data);
-        shift_west(&mut data);
-        shift_south(&mut data);
-        shift_east(&mut data);
+    for cycle in 0usize..(LIMIT_CYCLE_OFFSET + LIMIT_CYCLE_LENGTH) {
+        shift_rocks(&mut data, ShiftDirection::NORTH);
+        shift_rocks(&mut data, ShiftDirection::WEST);
+        shift_rocks(&mut data, ShiftDirection::SOUTH);
+        shift_rocks(&mut data, ShiftDirection::EAST);
 
-        if cycle >= 92 {
-            cache.insert(cycle - 92, data.clone());
+        if cycle >= LIMIT_CYCLE_OFFSET {
+            cache.insert(cycle - LIMIT_CYCLE_OFFSET, data.clone());
         }
     }
 
-    let cache_cycle = (1000000000usize - 1 - 92) % 72;
+    //Predict with the final rock layout using the logic outlined above
+    let cache_cycle = (NUM_CYCLES - 1 - LIMIT_CYCLE_OFFSET) % LIMIT_CYCLE_LENGTH;
     let final_data = &cache[&cache_cycle];
-
-    //print_debug(&data);
-
 
     //Calculate load and print answer
     let total_rock_load = (0..final_data.len())
@@ -60,95 +57,41 @@ fn main() {
 }
 
 fn parse_data(path: &Path) -> Vec<Vec<char>> {
-let file = File::open(&path).unwrap();
-return BufReader::new(file)
-   .lines()
-   .flatten()
-   .into_iter()
-   .map(|line| line.chars().collect::<Vec<char>>())
-   .collect();
+    let file = File::open(&path).unwrap();
+    return BufReader::new(file)
+       .lines()
+       .flatten()
+       .into_iter()
+       .map(|line| line.chars().collect::<Vec<char>>())
+       .collect();
 }
 
-fn print_debug(data: &Vec<Vec<char>>) {
-for row in data.iter() {
-   for element in row.iter() {
-       print!("{}", element);
-   }
-   print!("\n");
-}
-}
+fn shift_rocks(data: &mut Vec<Vec<char>>, shift_direction: ShiftDirection) {
+    let (row_range, col_range, row_shift_offset, col_shift_offset): (Range<isize>, Range<isize>, isize, isize) = match shift_direction {
+        ShiftDirection::NORTH => (1..data.len() as isize, 0..data[0].len() as isize, -1, 0),
+        ShiftDirection::WEST => (0..data.len() as isize, 1..data[0].len() as isize, 0, -1),
+        ShiftDirection::SOUTH => (0..data.len() as isize - 1, 0..data[0].len() as isize, 1, 0),
+        ShiftDirection::EAST => (0..data.len() as isize, 0..data[0].len() as isize - 1, 0, 1)
+    };
 
-fn shift_north(data: &mut Vec<Vec<char>>) {
-let mut rock_shifted = true;
+    let mut rock_shifted = true;
+    while rock_shifted {
+        rock_shifted = false;
 
-while (rock_shifted) {
-   rock_shifted = false;
-
-   //Rectangular input so using first row arbitrarily for max column index
-   for (row_index, col_index) in (1..data.len()).cartesian_product(0..data[0].len()) {
-       if data[row_index][col_index] == MOVABLE && data[row_index - 1][col_index] == EMPTY {
-           data[row_index][col_index] = EMPTY;
-           data[row_index - 1][col_index] = MOVABLE;
-           rock_shifted = true;
-       }
-   }
-}
-}
-
-fn shift_west(data: &mut Vec<Vec<char>>) {
-let mut rock_shifted = true;
-
-while (rock_shifted) {
-   rock_shifted = false;
-
-   //Rectangular input so using first row arbitrarily for max column index
-   for (row_index, col_index) in (0..data.len()).cartesian_product(1..data[0].len()) {
-       if data[row_index][col_index] == MOVABLE && data[row_index][col_index - 1] == EMPTY {
-           data[row_index][col_index] = EMPTY;
-           data[row_index][col_index - 1] = MOVABLE;
-           rock_shifted = true;
-       }
-   }
-}
-}
-
-fn shift_south(data: &mut Vec<Vec<char>>) {
-let mut rock_shifted = true;
-
-while (rock_shifted) {
-   rock_shifted = false;
-
-   //Rectangular input so using first row arbitrarily for max column index
-   for (row_index, col_index) in (0..data.len() - 1).cartesian_product(0..data[0].len()) {
-       if data[row_index][col_index] == MOVABLE && data[row_index + 1][col_index] == EMPTY {
-           data[row_index][col_index] = EMPTY;
-           data[row_index + 1][col_index] = MOVABLE;
-           rock_shifted = true;
-       }
-   }
-}
-}
-
-fn shift_east(data: &mut Vec<Vec<char>>) {
-let mut rock_shifted = true;
-
-while (rock_shifted) {
-   rock_shifted = false;
-
-   //Rectangular input so using first row arbitrarily for max column index
-   for (row_index, col_index) in (0..data.len()).cartesian_product(0..data[0].len() - 1) {
-       if data[row_index][col_index] == MOVABLE && data[row_index][col_index + 1] == EMPTY {
-           data[row_index][col_index] = EMPTY;
-           data[row_index][col_index + 1] = MOVABLE;
-           rock_shifted = true;
-       }
-   }
-}
+        //Rectangular input so using first row arbitrarily for max column index
+        for (row_index, col_index) in row_range.clone().cartesian_product(col_range.clone()) {
+            if data[row_index as usize][col_index as usize] == MOVABLE && data[(row_index + row_shift_offset) as usize][(col_index + col_shift_offset) as usize] == EMPTY {
+                data[row_index as usize][col_index as usize] = EMPTY;
+                data[(row_index + row_shift_offset) as usize][(col_index + col_shift_offset) as usize] = MOVABLE;
+                rock_shifted = true;
+            }
+        }
+    }
 }
 
 fn calculate_rock_load(row_index: usize, col_index: usize, data: &Vec<Vec<char>>) -> usize {
-return match data[row_index][col_index] {
-   MOVABLE => data.len() - row_index,
-   _ => 0
-};
+    return match data[row_index][col_index] {
+       MOVABLE => data.len() - row_index,
+       _ => 0
+    };
 }
