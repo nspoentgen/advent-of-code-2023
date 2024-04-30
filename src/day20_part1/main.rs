723b8f7550f8e8fb24243c6cec3a14cc5dea6f66
Nick Spoentgen <157921556+nspoentgen@users.noreply.github.com>
2024-04-29 22:01:09 -0700

    Initial draft for day 20 part 1. First test input works but second fails.

--- src/day20_part1/main.rs
+++ src/day20_part1/main.rs
@@ -1,20 +1,50 @@
 use std::path::Path;
-use std::collections::{HashMap};
+use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use itertools::Itertools;
+use num_format::{Locale, ToFormattedString};
 
 use crate::modules::*;
 
 mod modules;
 
 fn main() {
+    let path = Path::new("src/day20_part1/test_input2.txt");
+    let mut downstream_modules = parse_data(&path);
+    println!("Modules:\n{:?}", downstream_modules.values().map(|x| x.clone()).collect_vec());
 
+    let mut low_pulse_count = 0u64;
+    let mut high_pulse_count = 0u64;
+
+    for _ in 0..1000 {
+        (low_pulse_count, high_pulse_count, downstream_modules) = process_one_cycle(low_pulse_count, high_pulse_count, downstream_modules);
+    }
+
+    println!("Low count = {:}, high count = {}", low_pulse_count.to_formatted_string(&Locale::en), high_pulse_count.to_formatted_string(&Locale::en));
 }
 
-fn parse_data(path: &Path) -> Vec<Box<dyn Module>> {
-    let mut modules = Vec::<Box<dyn Module>>::new();
+fn parse_data(path: &Path) -> HashMap<String, Box<dyn PulseReceiver>> {
+    //Init
+    let mut modules = HashMap::<String, Box<dyn PulseReceiver>>::new();
     let file = File::open(&path).unwrap();
+    let mut io_map = HashMap::<String, Vec<String>>::new();
+    let mut conjunction_destinations = HashMap::<String, Vec<String>>::new();
 
+    //Closure for storing relationships. Needed later for updating inputs
+    //for conjunctions
+    let mut update_io_map = |source: &str, destinations: &Vec<String>| {
+        for destination in destinations {
+            if !io_map.contains_key(destination) {
+                io_map.insert(destination.clone(), vec![source.to_string()]);
+            } else {
+                io_map.get_mut(destination).unwrap().push(source.to_string());
+            }
+        }
+    };
+
+    //Define each module. Conjunctions are special and are only partially defined.
+    //They will be updated in a following step.
     for line in BufReader::new(file).lines().flatten() {
         let line_split: Vec<&str> = line.split("->").collect();
         let mut module_type_definition = line_split[0];
@@ -25,14 +55,50 @@ fn parse_data(path: &Path) -> Vec<Box<dyn Module>> {
 
         module_type_definition = module_type_definition.trim();
         if module_type_definition == Broadcaster::NAME {
-            modules.push(Box::new(Broadcaster::new(&destinations)));
+            modules.insert(Broadcaster::NAME.to_string(), Box::new(Broadcaster::new(&destinations)));
+            update_io_map(Broadcaster::NAME, &destinations);
+
         } else if module_type_definition.chars().nth(0).unwrap() == '%' {
-            modules.push(Box::new(FlipFlop::new(&destinations)));
+            let name = &module_type_definition[1..];
+            modules.insert(name.to_string(), Box::new(FlipFlop::new(name, &destinations)));
+            update_io_map(name, &destinations);
+
         } else if module_type_definition.chars().nth(0).unwrap() == '&' {
-            modules.push(Box::new(Conjunction::new(&destinations)))
+            let name = &module_type_definition[1..];
+            update_io_map(name, &destinations);
+            conjunction_destinations.insert(name.to_string(), destinations);
         }
+    }
+
+    //Create conjunction modules
+    for (name, destinations) in &conjunction_destinations {
+        modules.insert(name.clone(), Box::new(Conjunction::new(name, &io_map[name], destinations)));
     }
 
     return modules;
 }
 
+fn process_one_cycle(initial_low_pulse_count: u64, initial_high_pulse_count: u64, mut downstream_modules: HashMap<String, Box<dyn PulseReceiver>>) -> (u64, u64, HashMap<String, Box<dyn PulseReceiver>>) {
+    let mut low_pulse_count = initial_low_pulse_count;
+    let mut high_pulse_count = initial_high_pulse_count;
+    let mut pulse_queue = VecDeque::<PulseOutput>::new();
+
+    //We always start with a single assumed button module and then go from there
+    let button_module = Button {};
+    let (low_pulses_generated, high_pulses_generated, output_pulses) = button_module.push();
+    low_pulse_count += low_pulses_generated;
+    high_pulse_count += high_pulses_generated;
+    pulse_queue.extend(output_pulses);
+
+    //Keep processing pulses as long as they are being generated. Process in FIFO order per
+    //problem statement
+    while pulse_queue.len() > 0 {
+        let pulse = pulse_queue.pop_front().unwrap();
+        let (low_pulses_generated, high_pulses_generated, output_pulses) = downstream_modules.get_mut(&pulse.1).unwrap().process_input_pulse(&pulse.0, pulse.2);
+        low_pulse_count += low_pulses_generated;
+        high_pulse_count += high_pulses_generated;
+        pulse_queue.extend(output_pulses);
+    }
+
+    return (low_pulse_count, high_pulse_count, downstream_modules);
+}
--- src/day20_part1/modules.rs
+++ src/day20_part1/modules.rs
@@ -1,19 +1,21 @@
 use std::collections::{HashMap};
-use std::ops::Neg;
+use std::fmt::Debug;
+use std::ops::Not;
 use std::string::ToString;
+use itertools::Itertools;
 use PulseType::*;
 use SwitchState::*;
 
-#[derive(Clone, Copy, Eq, PartialEq)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 pub enum PulseType { Low, High }
 
-#[derive(Clone, Copy, Eq, PartialEq)]
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
 pub enum SwitchState { Off, On }
 
-impl Neg for SwitchState {
+impl Not for SwitchState {
     type Output = Self;
 
-    fn neg(self) -> Self::Output {
+    fn not(self) -> Self::Output {
         return match self {
             Off => On,
             On => Off
@@ -21,21 +23,24 @@ impl Neg for SwitchState {
     }
 }
 
-pub type PulseOutput = (String, PulseType);
+pub type PulseOutput = (String, String, PulseType);
 
-pub trait Module {
-    fn process_pulse(&mut self, source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>);
+pub trait PulseReceiver: Debug {
+    fn process_input_pulse(&mut self, source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>);
 }
 
+#[derive(Debug)]
 pub struct Button {}
 
 impl Button {
-    pub fn push() -> (u64, u64, Vec<PulseOutput>) {
-        return (1, 0, vec![(Broadcaster::NAME.to_string(), Low)])
+    pub fn push(&self) -> (u64, u64, Vec<PulseOutput>) {
+        return (1, 0, vec![("".to_string(), Broadcaster::NAME.to_string(), Low)])
     }
 }
 
+#[derive(Debug)]
 pub struct Broadcaster {
+    pub name: String,
     outputs: Vec<String>
 }
 
@@ -43,18 +48,20 @@ impl Broadcaster {
     pub const NAME: &'static str = "broadcaster";
 
     pub fn new(outputs: &Vec<String>) -> Self {
-        return Broadcaster { outputs: outputs.clone() };
+        return Broadcaster {
+            name: Broadcaster::NAME.to_string(),
+            outputs: outputs.clone() };
     }
 }
 
-impl Module for Broadcaster {
-    fn process_pulse(&mut self, source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>) {
+impl PulseReceiver for Broadcaster {
+    fn process_input_pulse(&mut self, _source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>) {
         let mut low_pulses_sent = 0u64;
         let mut high_pulses_sent = 0u64;
 
         let pulse_queue: Vec<PulseOutput> = self.outputs
             .iter()
-            .map(|x| (x.clone(), input_pulse))
+            .map(|x| (self.name.clone(), x.clone(), input_pulse))
             .collect();
 
         match input_pulse {
@@ -66,14 +73,21 @@ impl Module for Broadcaster {
     }
 }
 
+#[derive(Debug)]
 pub struct FlipFlop {
+    pub name: String,
     state: SwitchState,
     outputs: Vec<String>,
 }
 
 impl FlipFlop {
-    pub fn new(outputs: &Vec<String>) -> Self {
-        return FlipFlop {state: Off, outputs: outputs.clone() };
+    pub fn new(name: &str, outputs: &Vec<String>) -> Self {
+        const DEFAULT_STATE: SwitchState = Off;
+
+        return FlipFlop {
+            name: name.to_string(),
+            state: DEFAULT_STATE,
+            outputs: outputs.clone() };
     }
     
     pub fn state(&self) -> SwitchState {
@@ -81,20 +95,20 @@ impl FlipFlop {
     }
 }
 
-impl Module for FlipFlop {
-    fn process_pulse(&mut self, source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>) {
+impl PulseReceiver for FlipFlop {
+    fn process_input_pulse(&mut self, _source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>) {
         let mut low_pulses_sent = 0u64;
         let mut high_pulses_sent = 0u64;
         let mut pulse_queue = Vec::<PulseOutput>::new();
 
         if input_pulse == Low {
-            self.state != self.state;
+            self.state = !self.state;
 
             let emitted_pulse = match self.state {
                 Off => Low,
                 On => High
             };
-            pulse_queue.extend(self.outputs.iter().map(|x| (x.clone(), emitted_pulse)));
+            pulse_queue.extend(self.outputs.iter().map(|x| (self.name.clone(), x.clone(), emitted_pulse)));
 
             match emitted_pulse {
                 Low => low_pulses_sent += 1,
@@ -106,22 +120,28 @@ impl Module for FlipFlop {
     }
 }
 
+#[derive(Debug)]
 pub struct Conjunction {
+    name: String,
     inputs: HashMap<String, PulseType>,
     outputs: Vec<String>
 }
 
 impl Conjunction {
-    pub fn new(outputs: &Vec<String>) -> Self {
-        return Conjunction { inputs: HashMap::<String, PulseType>::new(), outputs: outputs.clone() };
+    pub fn new(name: &String, input_names: &Vec<String>, outputs: &Vec<String>) -> Self {
+        const DEFAULT_INPUT_STATE: PulseType = Low;
+
+        return Conjunction {
+            name: name.to_string(),
+            inputs: input_names.iter().map(|x| (x.clone(), DEFAULT_INPUT_STATE)).collect(),
+            outputs: outputs.clone() };
     }
 }
 
-impl Module for Conjunction {
-    fn process_pulse(&mut self, source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>) {
+impl PulseReceiver for Conjunction {
+    fn process_input_pulse(&mut self, source: &String, input_pulse: PulseType) -> (u64, u64, Vec<PulseOutput>) {
         let mut low_pulses_sent = 0u64;
         let mut high_pulses_sent = 0u64;
-        let mut pulse_queue = Vec::<PulseOutput>::new();
 
         *self.inputs.get_mut(source).unwrap() = input_pulse;
         let emitted_pulse = if self.inputs.iter().map(|x| x.1).all(|x| *x == High) {
@@ -130,9 +150,14 @@ impl Module for Conjunction {
             High
         };
 
+        let pulse_queue = self.outputs
+            .iter()
+            .map(|x| (self.name.clone(), x.clone(), emitted_pulse))
+            .collect_vec();
+
         match emitted_pulse {
-            Low => low_pulses_sent += 1,
-            High => high_pulses_sent += 1
+            Low => low_pulses_sent += self.outputs.len() as u64,
+            High => high_pulses_sent += self.outputs.len() as u64
         }
 
         return (low_pulses_sent, high_pulses_sent, pulse_queue);
