6233a1770e2349e7bda7e5323d8d43a257ac0653
Nick Spoentgen <157921556+nspoentgen@users.noreply.github.com>
2024-05-05 12:03:18 -0700

    Day 21 part 1 solution

--- src/day21_part1/main.rs
+++ src/day21_part1/main.rs
@@ -1,17 +1,14 @@
-use std::collections::hash_map::Entry;
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use itertools::Itertools;
 use num_format::{Locale, ToFormattedString};
 
-use crate::WalkResult::*;
-
 const START: char = 'S';
 const ROCK: char = '#';
 const GARDEN_PLOT: char = '.';
-const MAX_NUM_STEPS: u64 = 6;
+const MAX_NUM_STEPS: u64 = 64;
 
 //Convention: x-axis positive right, y-axis positive down
 type Coordinate = (usize, usize);
@@ -22,14 +19,9 @@ struct WalkState {
     pub steps_left: u64,
 }
 
-enum WalkResult<'a> {
-    Unsolved(),
-    Solved(&'a HashSet<Coordinate>)
-}
-
 fn main() {
     //Parse map
-    let path = Path::new("src/day21_part1/test_input.txt");
+    let path = Path::new("src/day21_part1/input.txt");
     let mut map = parse_data(&path);
 
     //Record starting position. Per problem statement, this is a garden
@@ -66,67 +58,43 @@ fn find_starting_position(data: &Vec<Vec<char>>) -> Coordinate {
 
 fn get_possible_num_positions(initial_position: Coordinate, map: &Vec<Vec<char>>) -> usize {
     let initial_state = WalkState { position: initial_position, steps_left: MAX_NUM_STEPS };
-    let initial_visited = HashSet::<Coordinate>::new();
-    let mut is_intial = true;
+    let mut final_positions = HashSet::<Coordinate>::new();
+    let mut solved_states = HashSet::<WalkState>::new();
 
-    let mut cache = HashMap::<WalkState, HashSet<Coordinate>>::new();
-    let mut work_queue = Vec::<(WalkState, HashSet<Coordinate>, bool)>::new();
-    work_queue.push((initial_state.clone(), initial_visited, true));
+    let mut work_queue = Vec::<WalkState>::new();
+    work_queue.push(initial_state.clone());
 
     while work_queue.len() > 0 {
         let next_work_item = work_queue.pop().unwrap();
-        let mut additional_work_items = walk(next_work_item.0, next_work_item.1, next_work_item.2, &mut cache, map);
+        let additional_work_items = walk(next_work_item, &mut final_positions, &mut solved_states, map);
         work_queue.extend(additional_work_items.into_iter().rev());
-        is_intial = false;
     }
 
-    return cache[&initial_state].len();
+    return final_positions.len();
 }
 
-fn walk(state: WalkState, mut visited: HashSet<Coordinate>, is_initial: bool, cache: &mut HashMap<WalkState, HashSet<Coordinate>>, map: &Vec<Vec<char>>) -> Vec<(WalkState, HashSet<Coordinate>, bool)> {
-    //Short-circuit if we already have answer
-    let cache_lookup = cache.get(&state);
-    if let Some(_) = cache_lookup {
-        return vec![];
-    }
-
-    if !is_initial {
-        visited.insert(state.position);
-    }
-    let mut walk_result;
+fn walk(state: WalkState, final_positions: &mut HashSet<Coordinate>, solved_states: &mut HashSet<WalkState>, map: &Vec<Vec<char>>) -> Vec<WalkState> {
+    let mut queued_work_items = vec![];
 
     if state.steps_left == 0 {
-        cache.insert(state.clone(), visited);
-        walk_result = vec![];
+        final_positions.insert(state.position);
+        solved_states.insert(state.clone());
     } else {
-        let mut child_results = Vec::<&HashSet<Coordinate>>::new();
-        let mut work_queue = Vec::<WalkState>::new();
         let next_positions = get_next_positions(state.position, map);
-
         for next_state in next_positions.iter().map(|p| WalkState {position: *p, steps_left: state.steps_left - 1 }) {
-            if let Some(cached_result) = cache.get(&next_state) {
-                child_results.push(cached_result);
-            } else {
-                work_queue.push(next_state);
+            if !solved_states.contains(&next_state) {
+                queued_work_items.push(next_state);
             }
         }
 
-        if work_queue.len() > 0 {
-            walk_result = work_queue.into_iter().map(|x| (x, visited.clone(), false)).collect_vec();
-            walk_result.push((state, visited, is_initial)); //don't forgot to re-add ourselves if we are not done
-
+        if queued_work_items.len() > 0 {
+            queued_work_items.push(state); //don't forgot to re-add ourselves if we are not done
         } else {
-            let mut combined_visited = HashSet::<Coordinate>::new();
-            for child_visited in child_results {
-                combined_visited.extend(child_visited);
-            }
-
-            cache.insert(state.clone(), visited);
-            walk_result = vec![];
+            solved_states.insert(state.clone());
         }
     }
 
-    return walk_result;
+    return queued_work_items;
 }
 
 fn get_next_positions(current_position: Coordinate, map: &Vec<Vec<char>>) -> Vec<Coordinate> {
