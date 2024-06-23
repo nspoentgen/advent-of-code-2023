9ef01cbf1c638096978b71cf1f1ac97168fdfcdf
Nick Spoentgen <157921556+nspoentgen@users.noreply.github.com>
2024-06-22 19:01:25 -0700

    Day 22 part 1 solution

--- src/day22_part1/main.rs
+++ src/day22_part1/main.rs
@@ -1,108 +1,31 @@
-use std::collections::{HashMap, HashSet};
+mod brick;
+
+use std::collections::{HashMap};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use itertools::Itertools;
-use num_format::{Locale, ToFormattedString};
-use num_format::Locale::{am, br, se};
+use brick::Orientation;
+use crate::brick::Brick;
 
 type Coordinate = (u64, u64, u64);
 
-#[derive(Eq, PartialEq, Hash)]
-enum Orientation {
-    X, Y, Z
-}
-
-#[derive(Eq, PartialEq, Hash)]
-struct Brick {
-    pub id: u32,
-    pub start_position: Coordinate,
-    pub end_position: Coordinate,
-    pub orientation: Orientation
-}
-
-impl Brick {
-    fn new(id: u32, start_position: Coordinate, end_position: Coordinate) -> Self {
-        let orientation = if start_position.0 == end_position.0 {
-            Orientation::X
-        } else if start_position.1 == end_position.1 {
-            Orientation::Y
-        } else {
-            Orientation::Z
-        };
-
-        return Brick { id, start_position, end_position, orientation};
-    }
-
-    fn can_fall_one(&self, world_map: &mut HashMap<Coordinate, u32>) -> bool {
-        if self.start_position.2 == 1 || self.end_position.2 == 1 {
-            return false;
-        }
-
-        let test_start_position = (self.start_position.0, self.start_position.1, self.start_position.2 - 1);
-        let start_result = world_map.get_key_value(&test_start_position);
-
-        let test_end_position = (self.end_position.0, self.end_position.1, self.end_position.2 - 1);
-        let end_result = world_map.get_key_value(&test_end_position);
-
-        return (start_result.is_none() || *start_result.unwrap().1 == self.id) &&
-            (end_result.is_none() || *end_result.unwrap().1 == self.id);
-    }
-
-    fn supports_a_brick(&self, world_map: &HashMap<Coordinate, u32>) -> bool {
-        let test_start_position = (self.start_position.0, self.start_position.1, self.start_position.2 + 1);
-        let start_result = world_map.get_key_value(&test_start_position);
-
-        let test_end_position = (self.end_position.0, self.end_position.1, self.end_position.2 + 1);
-        let end_result = world_map.get_key_value(&test_end_position);
-
-        return start_result.is_some_and(|x| *x.1 != self.id) &&
-            end_result.is_some_and(|x| *x.1 != self.id);
-    }
-
-    fn fall(&mut self, amount: u64, world_map: &mut HashMap<Coordinate, u32>) {
-        let old_start_position = self.start_position;
-        let old_end_position = self.end_position;
-        self.start_position.2 -= amount;
-        self.end_position.2 -= amount;
-
-        match self.orientation {
-            Orientation::X => {
-                for x in old_start_position.0..=old_end_position.0 {
-                    world_map.remove(&(x, old_start_position.1, old_start_position.2));
-                    world_map.insert((x, self.start_position.1, self.start_position.2), self.id);
-                }
-            },
-            Orientation::Y => {
-                for y in old_start_position.1..=old_end_position.1 {
-                    world_map.remove(&(old_start_position.0, y, old_start_position.2));
-                    world_map.insert((self.start_position.0, y, self.start_position.2), self.id);
-                }
-            },
-            Orientation::Z => {
-                for z in old_start_position.2..=old_end_position.2 {
-                    world_map.remove(&(old_start_position.0, old_start_position.1, z));
-                    world_map.insert((self.start_position.0, self.start_position.1, z - amount), self.id);
-                }
-            }
-        };
-    }
-}
-
-
 fn main() {
     //Parse data and init world map
-    let path = Path::new("src/day22_part1/test_input.txt");
+    let path = Path::new("src/day22_part1/input.txt");
     let mut bricks = parse_data(&path);
     let mut world_map = HashMap::<Coordinate, u32>::new();
     initialize_world_map(&bricks, &mut world_map);
 
     //Let bricks fall and then disintegrate
     get_final_positions(&mut bricks, &mut world_map);
-    let mut disintegrated_bricks = HashSet::<Brick>::from_iter(bricks.into_iter());
-    disintegrate_bricks(&mut disintegrated_bricks, &mut world_map);
+    let num_bricks_can_be_removed = bricks
+        .iter()
+        .map(|x| brick_can_be_disintegrated(x, &bricks, &mut world_map))
+        .filter(|x| *x)
+        .count();
 
-    println!("The number of bricks is {}", disintegrated_bricks.len());
+    println!("The number of bricks that can be removed is {}", num_bricks_can_be_removed);
 }
 
 fn parse_data(path: &Path) -> Vec<Brick> {
@@ -127,24 +50,48 @@ fn parse_positions(line: String) -> Vec<Coordinate> {
 
 fn initialize_world_map(bricks: &Vec<Brick>, world_map: &mut HashMap<Coordinate, u32>) {
     for brick in bricks {
-        match brick.orientation {
-            Orientation::X => {
-                for x in brick.start_position.0..=brick.end_position.0 {
-                    world_map.insert((x, brick.start_position.1, brick.start_position.2), brick.id);
-                }
-            },
-            Orientation::Y => {
-                for y in brick.start_position.1..=brick.end_position.1 {
-                    world_map.insert((brick.start_position.0, y, brick.start_position.2), brick.id);
-                }
-            },
-            Orientation::Z => {
-                for z in brick.start_position.2..=brick.end_position.2 {
-                    world_map.insert((brick.start_position.0, brick.start_position.1, z), brick.id);
-                }
-            }
-        };
+        insert_brick(&brick, world_map);
     }
+}
+
+fn insert_brick(brick: &Brick, world_map: &mut HashMap<Coordinate, u32>) {
+    match brick.orientation {
+        Orientation::X => {
+            for x in brick.start_position.0..=brick.end_position.0 {
+                world_map.insert((x, brick.start_position.1, brick.start_position.2), brick.id);
+            }
+        },
+        Orientation::Y => {
+            for y in brick.start_position.1..=brick.end_position.1 {
+                world_map.insert((brick.start_position.0, y, brick.start_position.2), brick.id);
+            }
+        },
+        Orientation::Z => {
+            for z in brick.start_position.2..=brick.end_position.2 {
+                world_map.insert((brick.start_position.0, brick.start_position.1, z), brick.id);
+            }
+        }
+    };
+}
+
+fn remove_brick(brick: &Brick, world_map: &mut HashMap<Coordinate, u32>) {
+    match brick.orientation {
+        Orientation::X => {
+            for x in brick.start_position.0..=brick.end_position.0 {
+                world_map.remove(&(x, brick.start_position.1, brick.start_position.2));
+            }
+        },
+        Orientation::Y => {
+            for y in brick.start_position.1..=brick.end_position.1 {
+                world_map.remove(&(brick.start_position.0, y, brick.start_position.2));
+            }
+        },
+        Orientation::Z => {
+            for z in brick.start_position.2..=brick.end_position.2 {
+                world_map.remove(&(brick.start_position.0, brick.start_position.1, z));
+            }
+        }
+    };
 }
 
 fn get_final_positions(bricks: &mut Vec<Brick>, world_map: &mut HashMap<Coordinate, u32>) {
@@ -162,23 +109,17 @@ fn get_final_positions(bricks: &mut Vec<Brick>, world_map: &mut HashMap<Coordina
     }
 }
 
-fn disintegrate_bricks(bricks: &mut HashSet<Brick>, world_map: &mut HashMap<Coordinate, u32>) {
-    let mut brick_disintegrated = true;
-    let mut bricks_to_remove = Vec::<&Brick>::new();
+fn brick_can_be_disintegrated(test_brick: &Brick, bricks: &Vec<Brick>, world_map: &mut HashMap<Coordinate, u32>) -> bool {
+    let mut can_be_removed = true;
+    remove_brick(&test_brick, world_map);
 
-    while brick_disintegrated {
-        brick_disintegrated = false;
-
-        for brick in bricks.iter() {
-            if brick.supports_a_brick(world_map) {
-                bricks_to_remove.push(brick);
-                brick_disintegrated = true;
-            }
+    for other_brick in bricks.iter().filter(|&x| *x != *test_brick) {
+        if other_brick.can_fall_one(world_map) {
+            can_be_removed = false;
+            break;
         }
-
-        for brick in bricks_to_remove {
-            bricks.remove(brick);
-        }
-        bricks_to_remove.clear();
     }
+
+    insert_brick(test_brick, world_map);
+    return can_be_removed;
 }
