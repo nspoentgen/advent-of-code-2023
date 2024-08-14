ec283dfafd6b55ec9e6ab5b0c14123f61471ff3e
Nick Spoentgen <157921556+nspoentgen@users.noreply.github.com>
2024-08-13 23:29:53 -0700

    Working through graph preprocessing

--- src/day23_part2/main.rs
+++ src/day23_part2/main.rs
@@ -15,6 +15,7 @@ fn main() {
     let path = Path::new("src/day23_part1/input.txt");
     let map = parse_data(&path);
     let (start_pos, end_pos) = find_terminal_positions(&map);
+    let preprocessed_graph = preprocess_graph(&map, &start_pos, &end_pos);
     let max_steps = get_max_steps(&start_pos, &end_pos, &map);
 
     println!("The maximum number of steps is {}", max_steps.to_formatted_string(&Locale::en));
@@ -40,13 +41,13 @@ fn find_terminal_positions(map: &Vec<Vec<char>>) -> ((usize, usize), (usize, usi
 
 
 
-fn get_max_steps(start_pos: &(usize, usize), end_pos: &(usize, usize), map: &Vec<Vec<char>>) -> usize {
+fn get_max_steps(start_pos: &(usize, usize), end_pos: &(usize, usize), preprocessed_graph: &HashMap<(usize, usize), Vec<((usize, usize), usize)>>) -> usize {
     //DFS
     let mut max_steps = 0usize;
     let mut work_stack = Vec::<WorkItem>::new();
     let mut initial_visited = HashSet::<(usize, usize)>::new();
     initial_visited.insert(start_pos.clone());
-    work_stack.extend(generate_work_items(start_pos, map, &initial_visited));
+    work_stack.extend(generate_work_items(start_pos, preprocessed_graph, &initial_visited));
 
     //let mut iteration = 0usize;
 
@@ -62,17 +63,17 @@ fn get_max_steps(start_pos: &(usize, usize), end_pos: &(usize, usize), map: &Vec
                 max_steps = work_item.1.len() - 1; //-1 to account for fact that first tile doesn't count as a step
             }
         } else {
-            work_stack.extend(generate_work_items(&work_item.0, map, &work_item.1))
+            work_stack.extend(generate_work_items(&work_item.0, preprocessed_graph, &work_item.1))
         }
     }
 
     return max_steps;
 }
 
-fn generate_work_items(current_pos: &(usize, usize), map: &Vec<Vec<char>>, visited: &HashSet<(usize, usize)>) -> Vec<WorkItem> {
+fn generate_work_items(current_pos: &(usize, usize), preprocessed_graph: &HashMap<(usize, usize), Vec<((usize, usize), usize)>>, visited: &HashSet<(usize, usize)>) -> Vec<WorkItem> {
     let mut work_items = Vec::<WorkItem>::new();
 
-    for valid_move in get_valid_preprocessed_moves(current_pos, map, visited) {
+    for valid_move in get_valid_moves_without_revisit(current_pos, preprocessed_graph, visited) {
         let mut child_visited = visited.clone();
         child_visited.insert(current_pos.clone());
         work_items.push((valid_move, child_visited));
@@ -84,10 +85,16 @@ fn generate_work_items(current_pos: &(usize, usize), map: &Vec<Vec<char>>, visit
 fn preprocess_graph(map: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos: &(usize, usize)) -> HashMap<(usize, usize), Vec<((usize, usize), usize)>> {
     let mut preprocess_graph = HashMap::<(usize, usize), Vec<((usize, usize), usize)>>::new();
     let junction_tiles = find_junction_tiles(map, start_pos, end_pos);
+
+    for tile in &junction_tiles {
+        update_preprocessed_graph(tile, &junction_tiles, start_pos, map, &mut preprocess_graph);
+    }
+
+    return preprocess_graph;
 }
 
-fn find_junction_tiles(map: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos: &(usize, usize)) -> Vec<(usize, usize)> {
-    let mut junction_tiles = Vec::<(usize, usize)>::new();
+fn find_junction_tiles(map: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos: &(usize, usize)) -> HashSet<(usize, usize)> {
+    let mut junction_tiles = HashSet::<(usize, usize)>::new();
 
     for row in 0..map.iter().len() {
         for col in 0..map[row].iter().len() {
@@ -95,7 +102,7 @@ fn find_junction_tiles(map: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos
                 (row, col) == *end_pos ||
                 get_valid_moves(&(row, col), map).len() > 1 {
 
-                junction_tiles.push((row, col));
+                junction_tiles.insert((row, col));
             }
         }
     }
@@ -103,25 +110,67 @@ fn find_junction_tiles(map: &Vec<Vec<char>>, start_pos: &(usize, usize), end_pos
     return junction_tiles;
 }
 
-fn get_valid_moves(position: &(usize, usize), map: &Vec<Vec<char>>) -> HashSet<(usize, usize)> {
+fn update_preprocessed_graph(tile_position: &(usize, usize), junction_tiles: &HashSet<(usize, usize)>, start_pos: &(usize, usize), map: &Vec<Vec<char>>, path_lengths_map: &mut HashMap<(usize, usize), Vec<((usize, usize), usize)>>) {
+    for first_step_position in get_valid_moves(tile_position, map) {
+        let mut continue_walking_path = true;
+        let mut next_step = first_step_position;
+        let mut step_count = if first_step_position == *start_pos { 0usize } else { 1usize };
+
+        while continue_walking_path {
+            let valid_moves = get_valid_moves(&next_step, map);
+
+            match valid_moves.len() {
+                0 => continue_walking_path = false,
+                1 => {
+                    if valid_moves[0] == *first_step_position {
+                        continue_walking_path  = false;
+                    } else if junction_tiles.contains(&valid_moves[0]) {
+                        if path_lengths_map.contains_key(&valid_moves[0]) {
+                            path_lengths_map[tile_position].push((valid_moves[0], step_count));
+                        } else {
+                            let path_lengths = vec![(valid_moves[0], step_count)];
+                            path_lengths_map[tile_position] = path_lengths;
+                        }
+
+                        continue_walking_path = false;
+                    } else {
+                        next_step = valid_moves[0];
+                    }
+                },
+                _ => {
+                    if path_lengths_map.contains_key(&valid_moves[0]) {
+                        path_lengths_map[tile_position].push((valid_moves[0], step_count));
+                    } else {
+                        let path_lengths = vec![(valid_moves[0], step_count)];
+                        path_lengths_map[tile_position] = path_lengths;
+                    }
+
+                    continue_walking_path = false;
+                }
+            }
+        }
+    }
+}
+
+fn get_valid_moves(position: &(usize, usize), map: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
     let deltas = [(0, -1), (0, 1), (-1, 0), (1, 0)];
 
     let signed_position = (position.0 as isize, position.1 as isize);
-    let mut valid_positions = HashSet::<(usize, usize)>::new();
+    let mut valid_positions = Vec::<(usize, usize)>::new();
 
     for delta in deltas {
         let test_position = (signed_position.0 + delta.0, signed_position.1 + delta.1);
         if test_position.0 >= 0 && test_position.0 < map.len() as isize && test_position.1 >= 0 &&
             test_position.1 < map[0].len() as isize && map[test_position.0 as usize][test_position.1 as usize] != FOREST
         {
-            valid_positions.insert( (test_position.0 as usize, test_position.1 as usize));
+            valid_positions.push( (test_position.0 as usize, test_position.1 as usize));
         }
     }
 
     return valid_positions;
 }
 
-fn get_valid_moves_no_overlap(position: &(usize, usize), map: &Vec<Vec<char>>, visited: &HashSet<(usize, usize)>) -> Vec<(usize, usize)> {
+fn get_valid_moves_without_revisit(position: &(usize, usize), preprocessed_graph: &HashMap<(usize, usize), Vec<((usize, usize), usize)>>, visited: &HashSet<(usize, usize)>) -> Vec<(usize, usize)> {
     let deltas = [(0, -1), (0, 1), (-1, 0), (1, 0)];
 
     let signed_position = (position.0 as isize, position.1 as isize);
